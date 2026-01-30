e4eef10419d97943934d850eb794604906497b3c
soundbar91 <soundbar91@jinhakapply.com>
2026-01-30 11:15:05 +0900

    fix: UserApi 삭제

--- naga-api/src/main/java/io/soundbar91/naga/user/controller/UserController.java
+++ naga-api/src/main/java/io/soundbar91/naga/user/controller/UserController.java
@@ -3,6 +3,7 @@ package io.soundbar91.naga.user.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +18,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Validated
-public class UserController implements UserApi {
+public class UserController {
 
     private final UserService userService;
 
-    @Override
+    @PostMapping
     public ResponseEntity<ApiResponse<UserResponse>> create(CreateUserRequest request) {
         User user = userService.create(request.email(), request.password());
         UserResponse response = UserResponse.from(user);
