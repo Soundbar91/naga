4e48abd685896d5c8cddca5e1d7096120738df5b
신관규 <soundbar91@gmail.com>
2026-07-01 14:04:51 +0900

    Initial commit

---
+++ pg-api/src/main/java/io/naga/pg/PgApiApplication.java
@@ -0,0 +1,13 @@
+package io.naga.pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgApiApplication.class, args);
+    }
+
+}
