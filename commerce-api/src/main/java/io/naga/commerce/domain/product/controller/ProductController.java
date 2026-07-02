c6ab55e1d613d61dc66a89f0ce97865770137cab
신관규 <soundbar91@gmail.com>
2026-07-03 02:14:48 +0900

    feat: 상품 목록 조회 API 추가

---
+++ commerce-api/src/main/java/io/naga/commerce/domain/product/controller/ProductController.java
@@ -0,0 +1,27 @@
+package io.naga.commerce.domain.product.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.naga.commerce.domain.product.dto.response.ProductResponse;
+import io.naga.commerce.domain.product.service.ProductService;
+import io.naga.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<ProductResponse>>> findProducts() {
+        List<ProductResponse> response = productService.findProducts();
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+}
