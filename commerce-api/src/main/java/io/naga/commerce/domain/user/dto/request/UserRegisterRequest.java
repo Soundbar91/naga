d40359881b353d0457a9d14303a361936f580bc5
신관규 <soundbar91@gmail.com>
2026-07-03 01:38:11 +0900

    fix: 커머스 API 테스트 데이터소스 설정

--- commerce-api/src/main/java/io/naga/commerce/domain/user/dto/request/UserRegisterRequest.java
+++ commerce-api/src/main/java/io/naga/commerce/domain/user/dto/request/UserRegisterRequest.java
@@ -1,12 +1,12 @@
 package io.naga.commerce.domain.user.dto.request;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 
 public record UserRegisterRequest(
-    @NotEmpty(message = "로그인 ID는 필수입니다.")
+    @NotBlank(message = "로그인 ID는 필수입니다.")
     String loginId,
 
-    @NotEmpty(message = "비밀번호는 필수입니다.")
+    @NotBlank(message = "비밀번호는 필수입니다.")
     String password
 ) {
 }
