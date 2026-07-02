fda4b518200efb11efb0cd4612cb90c34cead667
신관규 <soundbar91@gmail.com>
2026-07-03 01:45:34 +0900

    refactor: 회원가입 응답 타입 정리

--- commerce-api/src/main/java/io/naga/commerce/domain/user/controller/UserController.java
+++ commerce-api/src/main/java/io/naga/commerce/domain/user/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.naga.commerce.domain.user.dto.request.UserRegisterRequest;
 import io.naga.commerce.domain.user.service.UserService;
+import io.naga.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -19,7 +20,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<Void> registerUser(
+    public ResponseEntity<ApiResponse<Void>> registerUser(
         @Valid @RequestBody UserRegisterRequest request
     ) {
         userService.registerUser(request);
