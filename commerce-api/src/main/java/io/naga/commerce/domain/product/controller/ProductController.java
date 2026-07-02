afe6db35028617ce184e868111a23b1f5f2505e7
신관규 <soundbar91@gmail.com>
2026-07-03 02:15:17 +0900

    feat: 상품 상세 조회 API 추가

--- commerce-api/src/main/java/io/naga/commerce/domain/product/controller/ProductController.java
+++ commerce-api/src/main/java/io/naga/commerce/domain/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,14 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<ApiResponse<List<ProductResponse>>> findProducts() {
         List<ProductResponse> response = productService.findProducts();
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ApiResponse<ProductResponse>> findProduct(
+        @PathVariable Integer productId
+    ) {
+        ProductResponse response = productService.findProduct(productId);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
