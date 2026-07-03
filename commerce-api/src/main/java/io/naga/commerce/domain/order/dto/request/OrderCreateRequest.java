56fc8bd4816f232ecc0679b4e0345a1e8e5a13e1
신관규 <soundbar91@gmail.com>
2026-07-03 10:05:48 +0900

    refactor: 주문 요청 DTO 구조 정리

--- commerce-api/src/main/java/io/naga/commerce/domain/order/dto/request/OrderCreateRequest.java
+++ commerce-api/src/main/java/io/naga/commerce/domain/order/dto/request/OrderCreateRequest.java
@@ -4,10 +4,22 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record OrderCreateRequest(
     @Valid
     @NotEmpty(message = "주문 상품은 1개 이상이어야 합니다.")
-    List<OrderCreateItemRequest> items
+    List<InnerOrderCreateItemRequest> items
 ) {
+
+    public record InnerOrderCreateItemRequest(
+        @NotNull(message = "상품 ID는 필수입니다.")
+        Integer productId,
+
+        @NotNull(message = "주문 수량은 필수입니다.")
+        @Positive(message = "주문 수량은 1개 이상이어야 합니다.")
+        Integer quantity
+    ) {
+    }
 }
