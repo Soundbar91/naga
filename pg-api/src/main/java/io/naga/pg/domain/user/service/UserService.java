0cebc76078092646212b0c287a7ba9b9bdb6f0f8
신관규 <soundbar91@gmail.com>
2026-07-16 12:30:11 +0900

    refactor: 회원가입 저장 로직 단순화

--- pg-api/src/main/java/io/naga/pg/domain/user/service/UserService.java
+++ pg-api/src/main/java/io/naga/pg/domain/user/service/UserService.java
@@ -2,7 +2,6 @@ package io.naga.pg.domain.user.service;
 
 import static io.naga.common.error.ErrorCode.DUPLICATE_LOGIN_ID;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,18 +23,10 @@ public class UserService {
     @Transactional
     public void registerUser(UserRegisterRequest request) {
         if (userRepository.existsByLoginId(request.loginId())) {
-            throw duplicateLoginId(request.loginId());
+            throw BusinessException.of(DUPLICATE_LOGIN_ID, "loginId : " + request.loginId());
         }
 
         User user = User.create(request.loginId(), passwordEncoder.encode(request.password()));
-        try {
-            userRepository.saveAndFlush(user);
-        } catch (DataIntegrityViolationException exception) {
-            throw duplicateLoginId(request.loginId());
-        }
-    }
-
-    private BusinessException duplicateLoginId(String loginId) {
-        return BusinessException.of(DUPLICATE_LOGIN_ID, "loginId : " + loginId);
+        userRepository.save(user);
     }
 }
