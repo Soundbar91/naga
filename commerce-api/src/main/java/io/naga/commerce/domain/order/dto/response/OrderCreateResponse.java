1ea657d23242514f17799edd71af9d145a5b21d4
신관규 <soundbar91@gmail.com>
2026-07-21 05:53:44 +0900

    refactor: 주문 생성 응답 식별자 값 변경

--- commerce-api/src/main/java/io/naga/commerce/domain/order/dto/response/OrderCreateResponse.java
+++ commerce-api/src/main/java/io/naga/commerce/domain/order/dto/response/OrderCreateResponse.java
@@ -3,10 +3,10 @@ package io.naga.commerce.domain.order.dto.response;
 import io.naga.commerce.domain.order.model.Order;
 
 public record OrderCreateResponse(
-    Integer orderId
+    String orderId
 ) {
 
     public static OrderCreateResponse of(Order order) {
-        return new OrderCreateResponse(order.getId());
+        return new OrderCreateResponse(order.getOrderKey());
     }
 }
