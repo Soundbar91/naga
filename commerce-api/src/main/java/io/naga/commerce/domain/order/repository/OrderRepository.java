04802104fa0a4207e9a6e4e27166261e440aa96a
신관규 <soundbar91@gmail.com>
2026-07-21 05:56:33 +0900

    feat: 결제 승인 API 추가

--- commerce-api/src/main/java/io/naga/commerce/domain/order/repository/OrderRepository.java
+++ commerce-api/src/main/java/io/naga/commerce/domain/order/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package io.naga.commerce.domain.order.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.naga.commerce.domain.order.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    Optional<Order> findByOrderKeyAndUserId(String orderKey, Integer userId);
 }
---
+++ commerce-api/src/main/java/io/naga/commerce/domain/payment/service/PaymentService.java
@@ -0,0 +1,45 @@
+package io.naga.commerce.domain.payment.service;
+
+import static io.naga.common.error.ErrorCode.NOT_FOUND_ORDER;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.naga.commerce.domain.order.model.Order;
+import io.naga.commerce.domain.order.repository.OrderRepository;
+import io.naga.commerce.domain.payment.client.PgPaymentClient;
+import io.naga.commerce.domain.payment.dto.request.PaymentConfirmRequest;
+import io.naga.commerce.domain.payment.dto.response.PaymentConfirmResponse;
+import io.naga.commerce.domain.payment.model.Payment;
+import io.naga.commerce.domain.payment.repository.PaymentRepository;
+import io.naga.common.error.BusinessException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final OrderRepository orderRepository;
+    private final PgPaymentClient pgPaymentClient;
+    private final PaymentRepository paymentRepository;
+
+    @Transactional
+    public PaymentConfirmResponse confirmPayment(Integer userId, PaymentConfirmRequest request) {
+        Order order = orderRepository.findByOrderKeyAndUserId(request.orderId(), userId)
+            .orElseThrow(() -> BusinessException.of(NOT_FOUND_ORDER, "orderId : " + request.orderId()));
+        order.validatePayment(request.amount());
+
+        PaymentConfirmResponse response = pgPaymentClient.confirmPayment(request);
+        Payment payment = Payment.createApproved(
+            order,
+            response.paymentKey(),
+            response.amount(),
+            response.status(),
+            response.approvedAt()
+        );
+        paymentRepository.save(payment);
+        order.markAsPaid();
+
+        return response;
+    }
+}
